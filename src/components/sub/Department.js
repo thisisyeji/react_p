f8a0880fc12fc15ad0d118e243872cb46492e6e8
yjkim0109 <yjkkim0109@gmail.com>
2022-08-10 23:06:08 +0900

    feat: axios로 멤버 데이터 호출 및 출력

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,7 +1,36 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Department() {
-	return <Layout name={'Department'}>Department</Layout>;
+	const [Members, setMembers] = useState([]);
+
+	useEffect(() => {
+		axios.get(process.env.PUBLIC_URL + '/DB/members.json').then((json) => {
+			console.log(json.data.members);
+			setMembers(json.data.members);
+		});
+	}, []);
+
+	return (
+		<Layout name={'Department'}>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='img'>
+							<img
+								src={`${process.env.PUBLIC_URL}/img/${member.pic}`}
+								alt={member.name}
+							/>
+						</div>
+
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Department;
