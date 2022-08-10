5de6e7a95e5383088de39c980910641f1327759a
yjkim0109 <yjkkim0109@gmail.com>
2022-08-10 21:53:27 +0900

    feat: 추가 기본 라우터 연결 및 스위치로 메인,서브 헤더 분리

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Route, Switch } from 'react-router-dom';
+
 // common
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
@@ -16,16 +18,24 @@ import Youtube from './components/sub/Youtube';
 function App() {
 	return (
 		<>
-			<Header />
-			<Visual />
+			<Switch>
+				<Route exact path='/'>
+					<Header type={'main'} />
+					<Visual />
+				</Route>
 
-			<Department />
-			<Youtube />
-			<Gallery />
-			<Location />
-			<Community />
-			<Members />
-			<Footer />
+				<Route path='/'>
+					<Header type={'sub'} />
+				</Route>
+			</Switch>
+
+			<Route path='/department' component={Department} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/location' component={Location} />
+			<Route path='/community' component={Community} />
+			<Route path='/members' component={Members} />
+			<Route path='/footer' component={Footer} />
 		</>
 	);
 }
