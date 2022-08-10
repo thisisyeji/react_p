0e8e710d2417462935d614fdc1abcd0b3697677f
yjkim0109 <yjkkim0109@gmail.com>
2022-08-10 21:57:38 +0900

    feat: 추가헤더  기본 링크 연결

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,34 @@
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-	return <header>Header</header>;
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<ul id='gnb'>
+				<li>
+					<NavLink to='/department'>DEPARTMENT</NavLink>
+				</li>
+				<li>
+					<NavLink to='.youtube'>YOUTUBE</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery'>GALLERY</NavLink>
+				</li>
+				<li>
+					<NavLink to='/community'>COMMUNITY</NavLink>
+				</li>
+				<li>
+					<NavLink to='/location'>LOCATION</NavLink>
+				</li>
+				<li>
+					<NavLink to='/members'>MEMBERS</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
