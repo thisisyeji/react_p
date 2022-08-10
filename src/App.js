11fa5c9786b9887e017c76f669bce749faaf7815
yjkim0109 <yjkkim0109@gmail.com>
2022-08-10 22:05:00 +0900

    feat: 추가 기본 스타일링

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import Location from './components/sub/Location';
 import Members from './components/sub/Members';
 import Youtube from './components/sub/Youtube';
 
+import './scss/style.scss';
+
 function App() {
 	return (
 		<>
