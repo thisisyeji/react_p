32f79314d181483da060aa922143672d12705240
yjkim0109 <yjkkim0109@gmail.com>
2022-08-10 21:48:11 +0900

    feat: 추가 공통, 메인 서브 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+// common
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+
+// main
+import Visual from './components/main/Visual';
+
+//sub
+import Department from './components/sub/Department';
+import Community from './components/sub/Community';
+import Gallery from './components/sub/Gallery';
+import Location from './components/sub/Location';
+import Members from './components/sub/Members';
+import Youtube from './components/sub/Youtube';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+			<Visual />
+
+			<Department />
+			<Youtube />
+			<Gallery />
+			<Location />
+			<Community />
+			<Members />
+			<Footer />
+		</>
 	);
 }
 
