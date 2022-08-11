fd441cea56f27b89949a9b0dc34767d6ef958f5e
yjkim0109 <yjkkim0109@gmail.com>
2022-08-11 23:49:26 +0900

    feat: 추가 지도 마커 이미지

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -6,14 +6,21 @@ function Location() {
 	const { kakao } = window;
 	const container = useRef(null);
 	const opt = {
-		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		center: new kakao.maps.LatLng(37.51271224560607, 127.06069135102807),
 		level: 3,
 	};
 
+	// marker image
+	const imgSrc = process.env.PUBLIC_URL + '/img/marker1.png'; // 마커이미지
+	const imgSize = new kakao.maps.Size(223, 99); // 마커이미지 크기
+	const imgOpt = { offset: new kakao.maps.Point(116, 99) };
+
+	const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOpt);
+
 	// marker
-	const markerPosition = opt.center;
 	const marker = new kakao.maps.Marker({
-		position: markerPosition,
+		position: opt.center,
+		image: markerImg,
 	});
 
 	useEffect(() => {
