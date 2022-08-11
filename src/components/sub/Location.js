95d52ed381cf2ee8a00a82a172c76a20161f4a3d
yjkim0109 <yjkkim0109@gmail.com>
2022-08-12 00:22:46 +0900

    feat: 지도 교통정보량 표시 및 토글

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,5 +1,5 @@
 import Layout from '../common/Layout';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 function Location() {
 	// map
@@ -9,6 +9,11 @@ function Location() {
 		center: new kakao.maps.LatLng(37.51271224560607, 127.06069135102807),
 		level: 3,
 	};
+
+	// 교통정보
+	const [Traffic, setTraffic] = useState(null);
+	// 교통 정보 토글을 위한 불리언 값
+	const [TraToggle, setTraToggle] = useState(false);
 
 	// marker image
 	const imgSrc = process.env.PUBLIC_URL + '/img/marker1.png'; // 마커이미지
@@ -26,11 +31,22 @@ function Location() {
 	useEffect(() => {
 		const map = new kakao.maps.Map(container.current, opt);
 		marker.setMap(map);
+		setTraffic(map);
 	}, []);
+
+	useEffect(() => {
+		if (!Traffic) return;
+		TraToggle
+			? Traffic.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: Traffic.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	}, [TraToggle]);
 
 	return (
 		<Layout name={'Location'}>
 			<div className='map' ref={container}></div>
+			<button onClick={() => setTraToggle(!TraToggle)}>
+				{TraToggle ? 'Traffic Off' : 'Traffic On'}
+			</button>
 		</Layout>
 	);
 }
