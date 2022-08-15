aedf4e6ec59ac0d95921e0f49a1d526bc7003369
yjkim0109 <yjkkim0109@gmail.com>
2022-08-15 21:19:53 +0900

    feat: 추가 회원가입 checkbox, select, textarea 인증처리 및 모든 인증 통과 처리 완료

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -1,14 +1,22 @@
 import Layout from '../common/Layout';
-import { useState } from 'react';
-import { isValidElement } from 'react/cjs/react.production.min';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Members() {
+	const history = useHistory();
+
+	const [Submit, setSubmit] = useState(false);
+
+	// 기본 빈 객체
 	const initVal = {
 		userid: '',
 		email: '',
 		pwd1: '',
 		pwd2: '',
 		gender: null,
+		interests: null,
+		edu: '',
+		comments: '',
 	};
 	const [Val, setVal] = useState(initVal);
 	const [Err, setErr] = useState({});
@@ -48,6 +56,18 @@ function Members() {
 			errs.gender = '성별을 선택하세요.';
 		}
 
+		if (!value.interests) {
+			errs.interests = '관심사를 하나 이상 선택하세요.';
+		}
+
+		if (value.edu === '') {
+			errs.edu = '최종학력을 선택하세요.';
+		}
+
+		if (value.comments.length < 10) {
+			errs.comments = '코멘트를 10글자 이상 입력하세요.';
+		}
+
 		return errs;
 	};
 
@@ -63,12 +83,39 @@ function Members() {
 		setVal({ ...Val, [name]: value });
 	};
 
-	// gender 인풋 전용 함수
+	// radio 인풋 전용 함수
 	const handleRadio = (e) => {
 		const { name } = e.target;
 		const isCheck = e.target.checked;
 		setVal({ ...Val, [name]: isCheck });
 	};
+
+	// checkbox 인풋 전용 함수
+	const handleCheck = (e) => {
+		let isCheck = false;
+		const { name } = e.target;
+		const inputs = e.target.parentElement.querySelectorAll('input');
+		inputs.forEach((el) => {
+			if (el.checked) isCheck = true;
+		});
+
+		setVal({ ...Val, [name]: isCheck });
+	};
+
+	// select 인풋 전용 함수
+	const handleSelect = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
+	};
+
+	useEffect(() => {
+		const len = Object.keys(Err).length;
+		if (len === 0 && Submit) {
+			alert('회원가입이 완료되었습니다. 메인페이지로 이동합니다.');
+
+			history.push('/');
+		}
+	}, [Err]);
 
 	return (
 		<Layout name={'Members'}>
@@ -150,7 +197,7 @@ function Members() {
 
 							{/* gender */}
 							<tr>
-								<th>GENDER</th>
+								<th scope='row'>GENDER</th>
 
 								<td>
 									<input
@@ -173,11 +220,94 @@ function Members() {
 								</td>
 							</tr>
 
+							{/* interests */}
+							<tr>
+								<th scope='row'>INTERESTS</th>
+
+								<td>
+									<input
+										type='checkbox'
+										name='interests'
+										id='sports'
+										onChange={handleCheck}
+									/>
+									<label htmlFor='sports'>SPORTS</label>
+
+									<input
+										type='checkbox'
+										name='interests'
+										id='music'
+										onChange={handleCheck}
+									/>
+									<label htmlFor='music'>MUSIC</label>
+
+									<input
+										type='checkbox'
+										name='interests'
+										id='game'
+										onChange={handleCheck}
+									/>
+									<label htmlFor='game'>GAME</label>
+
+									<input
+										type='checkbox'
+										name='interests'
+										id='movie'
+										onChange={handleCheck}
+									/>
+									<label htmlFor='movie'>MOVIE</label>
+
+									<span className='err'>{Err.interests}</span>
+								</td>
+							</tr>
+
+							{/* edu */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='edu'>EDUCATION</label>
+								</th>
+
+								<td>
+									<select name='edu' id='edu' onChange={handleSelect}>
+										<option value=''>최종학력을 선택하세요.</option>
+										<option value='elementary-school'>초등학교 졸업</option>
+										<option value='middle-school'>중학교 졸업</option>
+										<option value='high-school'>고등학교 졸업</option>
+										<option value='college'>대학교 졸업</option>
+									</select>
+
+									<span className='err'>{Err.edu}</span>
+								</td>
+							</tr>
+
+							{/* comments */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='comments'>COMMENTS</label>
+								</th>
+
+								<td>
+									<textarea
+										name='comments'
+										id='comments'
+										cols='30'
+										rows='5'
+										placeholder='코멘트를 입력하세요'
+										onChange={handleChange}></textarea>
+
+									<span className='err'>{Err.comments}</span>
+								</td>
+							</tr>
+
 							{/* buttons */}
 							<tr>
 								<th colSpan='2'>
 									<input type='reset' value='CANCEL' />
-									<input type='submit' value='SUBMIT' />
+									<input
+										type='submit'
+										value='SUBMIT'
+										onClick={() => setSubmit(true)}
+									/>
 								</th>
 							</tr>
 						</tbody>
