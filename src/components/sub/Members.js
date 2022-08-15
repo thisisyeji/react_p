07591bfc893695e36582a1e51c3ef2e5047d5d35
yjkim0109 <yjkkim0109@gmail.com>
2022-08-15 20:48:14 +0900

    feat: 추가 회원가입 password, email, gender 인증처리

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -5,22 +5,47 @@ import { isValidElement } from 'react/cjs/react.production.min';
 function Members() {
 	const initVal = {
 		userid: '',
+		email: '',
+		pwd1: '',
+		pwd2: '',
+		gender: null,
 	};
 	const [Val, setVal] = useState(initVal);
 	const [Err, setErr] = useState({});
-
-	// input 값을 Val state에 저장하는 함수
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setVal({ ...Val, [name]: value });
-	};
 
 	// 인증 처리 함수
 	const check = (value) => {
 		const errs = {};
 
+		// pwd 정규식
+		const num = /[0-9]/;
+		const eng = /[a-zA-Z]/;
+		const spc = /[~!@#$%^&*()_+\[]/;
+
 		if (value.userid.length < 5) {
-			errs.userid = 'ID를 5글자 이상 입력하세요.';
+			errs.userid = '아이디를 5글자 이상 입력하세요.';
+		}
+
+		if (value.email.length < 6 || !/@/.test(value.email)) {
+			errs.email = '이메일을 @를 포함하여 입력하세요.';
+		}
+
+		if (
+			value.pwd1.length < 5 ||
+			!num.test(value.pwd1) ||
+			!eng.test(value.pwd1) ||
+			!spc.test(value.pwd1)
+		) {
+			errs.pwd1 =
+				'비밀번호를 5글자 이상 숫자, 영문자, 특수문자를 모두 포함하여 입력하세요.';
+		}
+
+		if (value.pwd1 !== value.pwd2 || !value.pwd2) {
+			errs.pwd2 = '비밀번호를 동일하게 입력하세요.';
+		}
+
+		if (!value.gender) {
+			errs.gender = '성별을 선택하세요.';
 		}
 
 		return errs;
@@ -30,6 +55,19 @@ function Members() {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		setErr(check(Val));
+	};
+
+	// input 값을 Val state에 저장하는 함수
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
+	};
+
+	// gender 인풋 전용 함수
+	const handleRadio = (e) => {
+		const { name } = e.target;
+		const isCheck = e.target.checked;
+		setVal({ ...Val, [name]: isCheck });
 	};
 
 	return (
@@ -51,10 +89,87 @@ function Members() {
 										type='text'
 										name='userid'
 										id='userid'
-										placeholder='ID를 입력하세요.'
+										placeholder='아이디를 입력하세요.'
 										onChange={handleChange}
 									/>
 									<span className='err'>{Err.userid}</span>
+								</td>
+							</tr>
+
+							{/* pwd */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd1'>PASSWORD</label>
+								</th>
+
+								<td>
+									<input
+										type='password'
+										name='pwd1'
+										id='pwd1'
+										placeholder='비밀번호를 입력하세요.'
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.pwd1}</span>
+								</td>
+							</tr>
+
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd2'>RE-PASSWORD</label>
+								</th>
+
+								<td>
+									<input
+										type='password'
+										name='pwd2'
+										id='pwd2'
+										placeholder='비밀번호를 재입력하세요.'
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.pwd2}</span>
+								</td>
+							</tr>
+
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>E-MAIL</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										name='email'
+										id='email'
+										placeholder='이메일을 입력하세요.'
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.email}</span>
+								</td>
+							</tr>
+
+							{/* gender */}
+							<tr>
+								<th>GENDER</th>
+
+								<td>
+									<input
+										type='radio'
+										name='gender'
+										id='male'
+										onChange={handleRadio}
+									/>
+									<label htmlFor='male'>MALE</label>
+
+									<input
+										type='radio'
+										name='gender'
+										id='female'
+										onChange={handleRadio}
+									/>
+									<label htmlFor='female'>FEMALE</label>
+
+									<span className='err'>{Err.gender}</span>
 								</td>
 							</tr>
 
