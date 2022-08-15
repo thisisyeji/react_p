fe6f24463b303d683f5dcb7597dd911fc97abdea
yjkim0109 <yjkkim0109@gmail.com>
2022-08-15 23:59:01 +0900

    feat: 추가 커뮤니티 기본 레이아웃

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,20 @@
 import Layout from '../common/Layout';
 
 function Community() {
-	return <Layout name={'Community'}>Community</Layout>;
+	return (
+		<Layout name={'Community'}>
+			<div className='input_box'>
+				<input type='text' placeholder='제목을 입력하세요.' /> <br />
+				<textarea
+					cols='30'
+					rows='3'
+					placeholder='내용을 입력하세요.'></textarea>{' '}
+				<br />
+				<button>CANCEL</button>
+				<button>WRITE</button>
+			</div>
+		</Layout>
+	);
 }
 
 export default Community;
