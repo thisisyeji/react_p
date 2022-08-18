ba8ccf3ae36f8d2dc777605a17044ce64b63f388
yjkim0109 <yjkkim0109@gmail.com>
2022-08-19 08:34:16 +0900

    feat: 추가 커뮤니티 글 수정 및 업데이트 기능

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,11 +1,18 @@
 import Layout from '../common/Layout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useRef, useState } from 'react';
 
 function Community() {
 	const input = useRef(null);
 	const textarea = useRef(null);
+
+	const inputEdit = useRef(null);
+	const textareaEdit = useRef(null);
+
 	const [Posts, setPosts] = useState([]);
+	const [Allowed, setAllowed] = useState(true);
 
 	//글 저장 함수
 	const createPost = () => {
@@ -32,32 +39,112 @@ function Community() {
 		setPosts(newPosts);
 	};
 
+	// 글 수정모드 변경 함수
+	const enableUpdate = (index) => {
+		if (!Allowed) return;
+		setAllowed(false);
+
+		setPosts(
+			Posts.map((post, idx) => {
+				if (idx === index) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
+	// 출력 모드 변경
+	const disableUpdate = (index) => {
+		setAllowed(true);
+		setPosts(
+			Posts.map((post, idx) => {
+				if (idx === index) post.enableUpdate = false;
+				return post;
+			})
+		);
+	};
+
+	// 실제 글 수정 함수
+	const updatePost = (index) => {
+		if (!inputEdit.current.value.trim() || !textareaEdit.current.value.trim()) {
+			resetForm();
+			return alert('Please type your new title and content');
+		}
+
+		setPosts(
+			Posts.map((post, idx) => {
+				if (idx === index) {
+					post.title = inputEdit.current.value;
+					post.content = textareaEdit.current.value;
+					post.enableUpdate = false;
+				}
+				return post;
+			})
+		);
+		setAllowed(true);
+	};
+
 	return (
 		<Layout name={'Community'}>
 			<div className='input_box'>
-				<input type='text' placeholder='제목을 입력하세요.' ref={input} />
+				<input type='text' placeholder='Title' ref={input} />
 				<br />
 				<textarea
 					cols='30'
 					rows='3'
-					placeholder='내용을 입력하세요.'
-					ref={textarea}></textarea>{' '}
+					placeholder='Content'
+					ref={textarea}></textarea>
 				<br />
-				<button>CANCEL</button>
-				<button onClick={createPost}>WRITE</button>
+				<div className='btns'>
+					<button onClick={createPost}>
+						<FontAwesomeIcon icon={faPen} />
+					</button>
+					<button>
+						<FontAwesomeIcon icon={faXmark} />
+					</button>
+				</div>
 			</div>
 
 			<div className='show_box'>
 				{Posts.map((post, idx) => {
 					return (
 						<article key={idx}>
-							<h3>{post.title}</h3>
-							<p>{post.content}</p>
-
-							<div className='btns'>
-								<button onClick={() => deletePost(idx)}>DELETE</button>
-								<button>EDIT</button>
-							</div>
+							{post.enableUpdate ? (
+								<>
+									<div className='editText'>
+										<input
+											type='text'
+											ref={inputEdit}
+											defaultValue={post.title}
+										/>
+										<br />
+										<textarea
+											cols='30'
+											rows='3'
+											ref={textareaEdit}
+											defaultValue={post.content}></textarea>
+										<br />
+										<div className='btns'>
+											<button onClick={() => updatePost(idx)}>
+												<FontAwesomeIcon icon={faCheck} />
+											</button>
+											<button onClick={() => disableUpdate(idx)}>
+												<FontAwesomeIcon icon={faXmark} />
+											</button>
+										</div>
+									</div>
+								</>
+							) : (
+								<>
+									<div className='txt'>
+										<h3>{post.title}</h3>
+										<p>{post.content}</p>
+									</div>
+									<div className='btns'>
+										<button onClick={() => enableUpdate(idx)}>EDIT</button>
+										<button onClick={() => deletePost(idx)}>DELETE</button>
+									</div>
+								</>
+							)}
 						</article>
 					);
 				})}
