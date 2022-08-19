bbf5a80bef0680e2299ede695630d0a43bfb4819
yjkim0109 <yjkkim0109@gmail.com>
2022-08-19 17:22:10 +0900

    feat: 추가 이미지, 비디오 캐싱처리

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,5 +1,13 @@
 function Visual() {
-	return <figure className='visual'></figure>;
+	return (
+		<figure className='visual'>
+			<video
+				src={process.env.PUBLIC_URL + '/img/vid0.mp4'}
+				muted
+				loop
+				autoPlay></video>
+		</figure>
+	);
 }
 
 export default Visual;
