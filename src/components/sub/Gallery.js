01e0b7cab7bddaabfa49640bdc5720a2a984c6a2
yjkim0109 <yjkkim0109@gmail.com>
2022-08-20 19:06:44 +0900

    feat: 갤러리 flickr 데이터 받아와서 기본 갤러리 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,7 +1,63 @@
 import Layout from '../common/Layout';
+import Popup from '../common/Popup';
+import axios from 'axios';
+import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
-	return <Layout name={'Gallery'}>Gallery</Layout>;
+	const key = 'ca6bb9623cb117b2c44bd339126530e9';
+	const method_gallery = 'flickr.galleries.getPhotos';
+	const gallery_id = '72157721034367990';
+	const num = 150;
+	const id = '196138805@N05';
+	const url_gallery = `https://www.flickr.com/services/rest/?method=${method_gallery}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&gallery_id=${gallery_id}`;
+
+	const [Items, setItems] = useState([]);
+	const frame = useRef(null);
+
+	const getFlickr = async (url) => {
+		await axios.get(url).then((json) => {
+			console.log(json.data.photos.photo);
+			setItems(json.data.photos.photo);
+		});
+		frame.current.classList.add('on');
+	};
+
+	useEffect(() => getFlickr(url_gallery), []);
+
+	return (
+		<>
+			<Layout name={'Gallery'}>
+				<div className='left'></div>
+
+				<div className='frame' ref={frame}>
+					{Items.map((pic, idx) => {
+						if (pic.title > 15) pic.title = pic.title.substring(0, 15) + '...';
+						return (
+							<article key={idx}>
+								<div className='pic_inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+											alt={pic.title}
+											onClick={() => {
+												setOpen(true);
+												setIndex(idx);
+											}}
+										/>
+									</div>
+									<h2>
+										{pic.title.length > 30
+											? pic.title.substr(0, 30) + '...'
+											: pic.title}
+									</h2>
+								</div>
+							</article>
+						);
+					})}
+				</div>
+			</Layout>
+		</>
+	);
 }
 
 export default Gallery;
