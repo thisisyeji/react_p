889b94234f757fde7cd5b710abf7d5ea00073a01
yjkim0109 <yjkkim0109@gmail.com>
2022-08-20 21:46:24 +0900

    feat: 추가 갤러리 masonry api

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,9 +1,18 @@
 import Layout from '../common/Layout';
 import Popup from '../common/Popup';
 import axios from 'axios';
+import Masonry from 'react-masonry-component';
 import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
+	const [Items, setItems] = useState([]);
+	const [Open, setOpen] = useState(false);
+	const [Index, setIndex] = useState(0);
+	const frame = useRef(null);
+
+	//masonry 전환속도 옵션객체 설정
+	const masonryOptions = { transitionDuration: '0.5s' };
+
 	const key = 'ca6bb9623cb117b2c44bd339126530e9';
 	const method_gallery = 'flickr.galleries.getPhotos';
 	const method_interest = 'flickr.interestingness.getList';
@@ -13,11 +22,6 @@ function Gallery() {
 	const url_gallery = `https://www.flickr.com/services/rest/?method=${method_gallery}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&gallery_id=${gallery_id}`;
 	const url_interest = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
 
-	const [Items, setItems] = useState([]);
-	const [Open, setOpen] = useState(false);
-	const [Index, setIndex] = useState(0);
-	const frame = useRef(null);
-
 	const getFlickr = async (url) => {
 		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
@@ -26,7 +30,13 @@ function Gallery() {
 		frame.current.classList.add('on');
 	};
 
-	useEffect(() => getFlickr(url_gallery), []);
+	useEffect(
+		() =>
+			setTimeout(() => {
+				getFlickr(url_gallery);
+			}, 500),
+		[]
+	);
 
 	return (
 		<>
@@ -49,30 +59,33 @@ function Gallery() {
 				</div>
 
 				<div className='frame' ref={frame}>
-					{Items.map((pic, idx) => {
-						if (pic.title > 15) pic.title = pic.title.substring(0, 15) + '...';
-						return (
-							<article key={idx}>
-								<div className='pic_inner'>
-									<div className='pic'>
-										<img
-											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-											alt={pic.title}
-											onClick={() => {
-												setOpen(true);
-												setIndex(idx);
-											}}
-										/>
+					<Masonry elementType='div' options={masonryOptions}>
+						{Items.map((pic, idx) => {
+							if (pic.title > 15)
+								pic.title = pic.title.substring(0, 15) + '...';
+							return (
+								<article key={idx}>
+									<div className='pic_inner'>
+										<div className='pic'>
+											<img
+												src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+												alt={pic.title}
+												onClick={() => {
+													setOpen(true);
+													setIndex(idx);
+												}}
+											/>
+										</div>
+										<h2>
+											{pic.title.length > 30
+												? pic.title.substr(0, 30) + '...'
+												: pic.title}
+										</h2>
 									</div>
-									<h2>
-										{pic.title.length > 30
-											? pic.title.substr(0, 30) + '...'
-											: pic.title}
-									</h2>
-								</div>
-							</article>
-						);
-					})}
+								</article>
+							);
+						})}
+					</Masonry>
 				</div>
 			</Layout>
 
