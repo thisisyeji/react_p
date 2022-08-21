a6390cdc9ce7cdbfd3984e737e96442e7f182d4f
yjkim0109 <yjkkim0109@gmail.com>
2022-08-21 21:05:09 +0900

    feat: 메인에 커뮤니티 게시글 미리보기 출력

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -14,39 +14,8 @@ function Community() {
 
 	// 로컬스토리지에 있는 데이터 가져오기
 	const getLocalData = () => {
-		const dummyPosts = [
-			{
-				title: 'What is Acqua di Parma’s mission?',
-				content:
-					'To share with the world the yellow gift of the Italian Sun, Soul, and Style.',
-			},
-			{
-				title: 'Scented celebrations of love',
-				content:
-					'The most special time of your life has met its scented partner.',
-			},
-			{
-				title: 'The ingredients of a product',
-				content: 'Where can I find the ingredients of a product?',
-			},
-			{
-				title: 'I love it.',
-				content:
-					"I purchased a product from your online Boutique. It's arrived beautifully packaged in very iconic yellow box. Grazie. ",
-			},
-			{
-				title: 'The luxury amenities are good.',
-				content:
-					'The hotel line includes a wide range of products, from bath and shower gel to hair products and body care.',
-			},
-		];
-
 		const data = localStorage.getItem('post');
-		if (data) {
-			return JSON.parse(data);
-		} else {
-			return dummyPosts;
-		}
+		return JSON.parse(data);
 	};
 
 	const [Posts, setPosts] = useState(getLocalData());
