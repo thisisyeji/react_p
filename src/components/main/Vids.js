395acb528f1bd7ee2324ed252fa06bc107634284
yjkim0109 <yjkkim0109@gmail.com>
2022-08-22 00:10:22 +0900

    feat: 추가 메인 스와이퍼

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,7 +1,44 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Pagination, Navigation } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
 function Vids() {
 	return (
 		<section id='Vids' className='myScroll'>
-			Vids
+			<Swiper
+				modules={[Pagination, Navigation]}
+				pagination={{ clickable: true }}
+				// navigation={true}
+				// 간격
+				spaceBetween={50}
+				// 순환
+				loop={true}
+				// 한번에 보여질 슬라이드 갯수
+				slidesPerView={3}
+				// 1번 슬라이드를 센터로
+				centeredSlides={true}>
+				<SwiperSlide>
+					<div className='swipe_inner'>1</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='swipe_inner'>2</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='swipe_inner'>3</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='swipe_inner'>4</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='swipe_inner'>5</div>
+				</SwiperSlide>
+			</Swiper>
 		</section>
 	);
 }
