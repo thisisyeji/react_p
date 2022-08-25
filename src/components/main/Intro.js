a9a44c655810d1ebfbe8680422d6037945ca06f7
yjkim0109 <yjkkim0109@gmail.com>
2022-08-25 16:55:55 +0900

    feat: 추가 메인 인트로 버튼 스크롤 이벤트 및 키프레임

--- src/components/main/Intro.js
+++ src/components/main/Intro.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import Anime from '../../assets/Anime';
+import { useRef } from 'react';
 
 function Intro() {
 	const path = process.env.PUBLIC_URL;
+	const pos = useRef([]);
+	const boxRef = useRef(null);
+
+	// 세로 스크롤 위치값 구하는 함수
+	const getPos = () => {
+		pos.current = [];
+		const sections = boxRef.current.parentElement.querySelectorAll('.myScroll');
+		for (const sec of sections) pos.current.push(sec.offsetTop);
+	};
+
 	return (
-		<section id='Intro' className='myScroll'>
+		<section id='Intro' className='myScroll' ref={boxRef}>
 			<div className='inner'>
 				<h2>
 					<div className='title_first'>
@@ -20,7 +31,18 @@ function Intro() {
 					</div>
 					<span className='title_last'>AMAZING FRAGRANCE</span>
 				</h2>
-				<div className='btn'>Explore</div>
+				<div
+					className='btn'
+					onClick={() => {
+						getPos();
+						new Anime(window, {
+							prop: 'scroll',
+							value: pos.current[1],
+							duration: 500,
+						});
+					}}>
+					Explore
+				</div>
 			</div>
 		</section>
 	);
