a0be759766632c3b81d455bbd16b12de3b7b51d6
yjkim0109 <yjkkim0109@gmail.com>
2022-08-26 23:37:45 +0900

    feat: 메인 Vids 스와이퍼 유튜브 불러오기 및 변경

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,7 +1,9 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import required modules
-import { Pagination, Navigation } from 'swiper';
+import { Pagination, Navigation, Autoplay } from 'swiper';
+
+import { useSelector } from 'react-redux';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -9,35 +11,38 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 function Vids() {
+	const Videos = useSelector((store) => store.youtubeReducer.youtube);
+
 	return (
 		<section id='Vids' className='myScroll'>
 			<Swiper
-				modules={[Pagination, Navigation]}
+				modules={[Pagination, Navigation, Autoplay]}
 				pagination={{ clickable: true }}
-				// navigation={true}
+				navigation={true}
 				// 간격
-				spaceBetween={50}
+				spaceBetween={40}
 				// 순환
 				loop={true}
 				// 한번에 보여질 슬라이드 갯수
 				slidesPerView={3}
 				// 1번 슬라이드를 센터로
 				centeredSlides={true}>
-				<SwiperSlide>
-					<div className='swipe_inner'>1</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div className='swipe_inner'>2</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div className='swipe_inner'>3</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div className='swipe_inner'>4</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div className='swipe_inner'>5</div>
-				</SwiperSlide>
+				{Videos.map((data, idx) => {
+					if (idx >= 4) return;
+					return (
+						<SwiperSlide key={data.id}>
+							<div className='inner'>
+								<div className='pic'>
+									<img
+										src={data.snippet.thumbnails.maxres.url}
+										alt={data.snippet.title}
+									/>
+								</div>
+								<h3>{data.snippet.title}</h3>
+							</div>
+						</SwiperSlide>
+					);
+				})}
 			</Swiper>
 		</section>
 	);
