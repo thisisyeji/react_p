49fd727418cbc8a8c1c6b303230e6c2d98a693ce
yjkim0109 <yjkkim0109@gmail.com>
2022-08-28 00:53:04 +0900

    feat: 메인 Pics 레이아웃 및 스타일링 추가

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,7 +1,42 @@
 function Pics() {
+	const path = process.env.PUBLIC_URL;
 	return (
 		<section id='Pics' className='myScroll'>
-			Pics
+			<h2>
+				<span>MAGNOLIA INFINITA</span>
+				<span>MAGNOLIA INFINITA</span>
+				<span>MAGNOLIA INFINITA</span>
+			</h2>
+
+			<div className='start'>
+				<div className='img'>
+					<img src={path + '/img/f1.jpg'} />
+				</div>
+
+				<h3>MEET OUR NEW NEW MAGNOLIA INFINITA</h3>
+				<p>
+					Signatures of the Sun welcomes the new Magnolia Infinita, a continuous
+					blossoming of wonders that never ceases to amaze
+				</p>
+			</div>
+
+			<div className='product fragrance'>
+				<div className='img'>
+					<img src={path + '/img/f.jpg'} />
+				</div>
+				<div className='detail'>
+					<h3>NATURAL SPRAY</h3>
+					<p>
+						This new fragrance is an artistic combination of two olfactive
+						sensations: the infinite floral scent of Magnolia and vibrant
+						luminous citrus notes. The elegant and floral scent of this powerful
+						flower is combined with top notes of Calabrian Bergamot, Orange, and
+						Lemon to introduce vivid rays of light to the fascinating and fresh
+						appeal of the Magnolia flower.
+					</p>
+					<button>MORE</button>
+				</div>
+			</div>
 		</section>
 	);
 }
