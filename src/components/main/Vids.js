d2c9b002e57ccba2c88ed9be07d368aa00c4c895
yjkim0109 <yjkkim0109@gmail.com>
2022-08-27 18:03:59 +0900

    feat: 추가 메인 Vids 팝업 및 클릭 이벤트

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -4,6 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation, Autoplay } from 'swiper';
 
 import { useSelector } from 'react-redux';
+import { useState, useRef } from 'react';
+import Popup from '../common/Popup';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -14,53 +16,72 @@ function Vids({ Scrolled, currentPos }) {
 	const Videos = useSelector((store) => store.youtubeReducer.youtube);
 
 	const position = Scrolled - currentPos || 0;
+	const pop = useRef(null);
+	const [Index, setIndex] = useState(0);
 
 	return (
-		<section id='Vids' className='myScroll'>
-			<h2 className='hidden'>Youtube</h2>
-			<div className='banner'>
-				<span style={{ marginLeft: position * -2 }}>
-					Acqua di Parma, a symbol of Italian savoir-faire and refinement,
-				</span>
-			</div>
-			<Swiper
-				modules={[Pagination, Navigation, Autoplay]}
-				// pagination={{ clickable: true }}
-				navigation={true}
-				// 오토 플레이
-				autoplay={{ delay: 3000, disableOnInteraction: false }}
-				// 간격
-				spaceBetween={40}
-				// 순환
-				loop={true}
-				// 한번에 보여질 슬라이드 갯수
-				slidesPerView={3}
-				// 1번 슬라이드를 센터로
-				centeredSlides={true}>
-				{Videos.map((data, idx) => {
-					return (
-						<SwiperSlide key={data.id}>
-							<div className='inner'>
-								<div className='pic'>
-									<img
-										src={data.snippet.thumbnails.maxres.url}
-										alt={data.snippet.title}
-									/>
-									<div className='cover'>VIEW</div>
+		<>
+			<section id='Vids' className='myScroll'>
+				<h2 className='hidden'>Youtube</h2>
+				<div className='banner'>
+					<span style={{ marginLeft: position * -2 }}>
+						Acqua di Parma, a symbol of Italian savoir-faire and refinement,
+					</span>
+				</div>
+				<Swiper
+					modules={[Pagination, Navigation, Autoplay]}
+					// pagination={{ clickable: true }}
+					navigation={true}
+					// 오토 플레이
+					autoplay={{ delay: 3000, disableOnInteraction: false }}
+					// 간격
+					spaceBetween={40}
+					// 순환
+					loop={true}
+					// 한번에 보여질 슬라이드 갯수
+					slidesPerView={3}
+					// 1번 슬라이드를 센터로
+					centeredSlides={true}>
+					{Videos.map((data, idx) => {
+						return (
+							<SwiperSlide key={data.id}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={data.snippet.thumbnails.maxres.url}
+											alt={data.snippet.title}
+										/>
+										<div
+											className='cover'
+											onClick={() => {
+												setIndex(idx);
+												pop.current.open();
+											}}>
+											VIEW
+										</div>
+									</div>
+									<h3>{data.snippet.title}</h3>
 								</div>
-								<h3>{data.snippet.title}</h3>
-							</div>
-						</SwiperSlide>
-					);
-				})}
-			</Swiper>
-			<div className='banner'>
-				<span style={{ marginLeft: position * 2 }} className='banner_bottom'>
-					embodies discreet luxury through its elegant fragrances and lifestyle
-					products.
-				</span>
-			</div>
-		</section>
+							</SwiperSlide>
+						);
+					})}
+				</Swiper>
+				<div className='banner'>
+					<span style={{ marginLeft: position * 2 }} className='banner_bottom'>
+						embodies discreet luxury through its elegant fragrances and
+						lifestyle products.
+					</span>
+				</div>
+			</section>
+			<Popup ref={pop}>
+				{Videos.length !== 0 && (
+					<iframe
+						src={`https://www.youtube.com/embed/${Videos[Index].snippet.resourceId.videoId}`}
+						frameBorder='0'
+						allowFullScreen></iframe>
+				)}
+			</Popup>
+		</>
 	);
 }
 
